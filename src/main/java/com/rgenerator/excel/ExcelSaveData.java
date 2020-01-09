b779d2faab90cfadac1314dccb5a1860aef7c0f8
Vitalii Pidhoretskyi <pidhoretskyi.jr@gmail.com>
2020-01-09 13:43:41 +0200

    header_added

--- src/main/java/com/rgenerator/excel/ExcelSaveData.java
+++ src/main/java/com/rgenerator/excel/ExcelSaveData.java
@@ -4,7 +4,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 
 import com.ibm.db2.jcc.am.Connection;
 import com.ibm.db2.jcc.am.ResultSet;
@@ -17,10 +19,12 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
@@ -28,86 +32,111 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 public class ExcelSaveData {
-	
-	
-	public void SaveACC(Workbook workbook, ResultSet ACCdata, String ACCname) {
+
+	public void SaveACC(Workbook workbook, ResultSet ACCdata, String ACCname, String reportName) {
 		try {
 			ResultSetMetaData data = ACCdata.getMetaData();
 			int columnCount = data.getColumnCount();
-			int currentRow = 0;
-		
-		
+			int currentRow = 4;
+
 			Sheet sheet = workbook.createSheet(ACCname);
+
+			Row rowReportName = sheet.createRow(0);
+			Row rowReportPeriod = sheet.createRow(1);
+			Row rowReportedASOn = sheet.createRow(2);
+
+			Cell cell1 = rowReportName.createCell(0);
+			Cell cell2 = rowReportPeriod.createCell(0);
+			Cell cell3 = rowReportedASOn.createCell(0);
 			
-			//Header style
+			HSSFRichTextString richTextStringRow1 = new HSSFRichTextString(
+					"Report Name : " + reportName);
+			HSSFRichTextString richTextStringRow2 = new HSSFRichTextString("Report Period (from-date, to-date):");
+			HSSFRichTextString richTextStringRow3 = new HSSFRichTextString("Reported As On : "
+					+ new SimpleDateFormat("dd.MM.yyyy HH.mm").format(Calendar.getInstance().getTime()));
+
+			Font fontBolt = workbook.createFont();
+			fontBolt.setBold(true);
+			
+			richTextStringRow1.applyFont(0, 12, fontBolt);
+			richTextStringRow2.applyFont(0, 35, fontBolt);
+			richTextStringRow3.applyFont(0, 17, fontBolt);
+			
+			cell1.setCellValue(richTextStringRow1);
+			cell2.setCellValue(richTextStringRow2);
+			cell3.setCellValue(richTextStringRow3);
+			
+			sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 4));
+			sheet.addMergedRegion(new CellRangeAddress(1, 1, 0, 4));
+			sheet.addMergedRegion(new CellRangeAddress(2, 2, 0, 4));
+			// Header style
 			Font headerFont = workbook.createFont();
 			headerFont.setBold(true);
-			headerFont.setFontHeightInPoints((short) 12);
+			headerFont.setFontHeightInPoints((short) 10);
 			headerFont.setColor(IndexedColors.BLACK.getIndex());
 			CellStyle headerCellStyle = workbook.createCellStyle();
 			headerCellStyle.setFont(headerFont);
-			
-			
-			//Date style
+			headerCellStyle.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
+			headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+			// Date style
 			CellStyle dateCellStyle = workbook.createCellStyle();
 			CreationHelper createHelper = workbook.getCreationHelper();
 			dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("DD.MM.YYYY"));
 			dateCellStyle.setAlignment(HorizontalAlignment.CENTER);
 			dateCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-				
-			//Datestamp style
+
+			// Datestamp style
 			CellStyle datestampCellStyle = workbook.createCellStyle();
 			datestampCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("DD.MM.YYYY HH:mm:ss.000"));
 			datestampCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 			datestampCellStyle.setAlignment(HorizontalAlignment.CENTER);
-			
-			//Text style
+
+			// Text style
 			CellStyle textCellStyle = workbook.createCellStyle();
 			textCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 			textCellStyle.setAlignment(HorizontalAlignment.CENTER);
-			
-        
-        
+
 			System.out.println("**** Writing to file");
 			Row row = sheet.createRow(currentRow++);
-			for(int i=1; i<=columnCount; i++) {
-				Cell cell = row.createCell(i-1);
+			for (int i = 1; i <= columnCount; i++) {
+				Cell cell = row.createCell(i - 1);
 				cell.setCellValue(data.getColumnName(i));
 				cell.setCellStyle(headerCellStyle);
 			}
-		  
+
 			boolean isDataExist = false;
-			while(ACCdata.next()) {
+			while (ACCdata.next()) {
 				isDataExist = true;
 				row = sheet.createRow(currentRow++);
-				for(int i=1; i<=columnCount; i++) {
-					Cell cell = row.createCell(i-1);
-					
-					if(data.getColumnType(i)==java.sql.Types.DATE) {
+				for (int i = 1; i <= columnCount; i++) {
+					Cell cell = row.createCell(i - 1);
+
+					if (data.getColumnType(i) == java.sql.Types.DATE) {
 						cell.setCellStyle(dateCellStyle);
 						cell.setCellValue(ACCdata.getDate(i));
-						
-					}else if(data.getColumnType(i)==java.sql.Types.TIMESTAMP) {
+
+					} else if (data.getColumnType(i) == java.sql.Types.TIMESTAMP) {
 						cell.setCellStyle(datestampCellStyle);
 						cell.setCellValue(ACCdata.getTimestamp(i));
-						
-						
-					}else {
+
+					} else {
 						cell.setCellValue(ACCdata.getString(i));
 						cell.setCellStyle(textCellStyle);
 					}
 				}
 			}
-			if(!isDataExist) {
+			if (!isDataExist) {
 				workbook.removeSheetAt(workbook.getSheetIndex(sheet));
 				return;
 			}
-			for(int i = 0; i < columnCount; i++) {
+			for (int i = 0; i < columnCount; i++) {
 				sheet.autoSizeColumn(i);
 			}
-		}catch (SQLException ex) {
+		} catch (SQLException ex) {
 			System.err.println("SQLException information");
 			while (ex != null) {
 				System.err.println("Error msg: " + ex.getMessage());
@@ -117,33 +146,34 @@ public class ExcelSaveData {
 				ex = ex.getNextException(); // For drivers that support chained exceptions
 			}
 		}
-        
+
 	}
-	
+
 	public void SaveHierarchy(String account) {
 		DbConnProvider server = new DbConnProvider();
 		Connection connection = server.openConn();
 		DbDataProvider dataProvider = new DbDataProvider(connection);
 		System.err.println("**** Saving hierarchy");
-		if(connection != null) {
+		if (connection != null) {
 			try {
 				ResultSet hier = dataProvider.GetHierarchy(account);
 				Workbook workbook = new HSSFWorkbook();
-				while(hier.next()) {
-					if(workbook.getSheet(hier.getString(1))!=null) continue;
+				while (hier.next()) {
+					if (workbook.getSheet(hier.getString(1)) != null)
+						continue;
 					ResultSet acc = dataProvider.MonthEntriesData(hier.getString(1));
-					SaveACC(workbook, acc, hier.getString(1));
+					SaveACC(workbook, acc, hier.getString(1), "Daily");
 				}
-				FileOutputStream fileOut = new FileOutputStream(account+"_"+LocalDate.now()+".xls");
+				FileOutputStream fileOut = new FileOutputStream(account + "_" + LocalDate.now() + ".xls");
 				workbook.write(fileOut);
 				fileOut.close();
-			}catch(IOException ex) {
-	        	System.err.println("IOException information");
-	        	while (ex != null) {
+			} catch (IOException ex) {
+				System.err.println("IOException information");
+				while (ex != null) {
 					System.err.println("Error msg: " + ex.getMessage());
-					
+
 				}
-			}catch (SQLException ex) {
+			} catch (SQLException ex) {
 				System.err.println("SQLException information");
 				while (ex != null) {
 					System.err.println("Error msg: " + ex.getMessage());
@@ -154,59 +184,59 @@ public class ExcelSaveData {
 				}
 			}
 		}
-		
-		
-		
+
 	}
-	
+
 	public void DailyEnries(String date) {
 		DbConnProvider server = new DbConnProvider();
 		Connection connection = server.openConn();
 		DbDataProvider dataProvider = new DbDataProvider(connection);
-		
-		
-		if(connection != null) {
-			
+
+		if (connection != null) {
+
 			try {
-				
+
 				ResultSet accountsForDailyReport = dataProvider.DailyEntriesACC(date);
 				Workbook workbook = new HSSFWorkbook();
-				FileOutputStream fileOut=null;
+				FileOutputStream fileOut = null;
 				int prevACChier = -1;
-				
-				while(accountsForDailyReport.next()) {
-					ResultSet accNumberData = dataProvider.getAccountNumberForDailyReport(accountsForDailyReport.getString(2));
-					
+
+				while (accountsForDailyReport.next()) {
+					ResultSet accNumberData = dataProvider
+							.getAccountNumberForDailyReport(accountsForDailyReport.getString(2));
+
 					accNumberData.next();
 					String accNumber = accNumberData.getString(2);
-					System.out.println("**** "+accNumber);
-					if(accNumber==null) continue;
-					if(workbook.getSheet(accNumber)!=null) continue; // if we already have sheet with current acc name when skip
-					if(prevACChier==accountsForDailyReport.getInt(1)) { //write data to the same hierarchy
+					System.out.println("**** " + accNumber);
+					if (accNumber == null)
+						continue;
+					if (workbook.getSheet(accNumber) != null)
+						continue; // if we already have sheet with current acc name when skip
+					if (prevACChier == accountsForDailyReport.getInt(1)) { // write data to the same hierarchy
 						ResultSet accData = dataProvider.DailyEntriesData(accountsForDailyReport.getString(2));
-						SaveACC(workbook, accData, accNumber);
-						prevACChier=accountsForDailyReport.getInt(1);
-					}else { //create new file for other hierarchy
-						if(workbook.getNumberOfSheets()!=0) {
-						fileOut = new FileOutputStream(workbook.getSheetName(0)+"_"+date+".xls");
-						workbook.write(fileOut);
-						fileOut.close();
+						SaveACC(workbook, accData, accNumber, "Daily");
+						prevACChier = accountsForDailyReport.getInt(1);
+					} else { // create new file for other hierarchy
+						if (workbook.getNumberOfSheets() != 0) {
+							fileOut = new FileOutputStream(workbook.getSheetName(0) + "_" + date + ".xls");
+							workbook.write(fileOut);
+							fileOut.close();
 						}
-					
+
 						workbook = new HSSFWorkbook();
 						ResultSet AccData = dataProvider.DailyEntriesData(accountsForDailyReport.getString(2));
-						SaveACC(workbook, AccData, accNumber);
-						prevACChier=accountsForDailyReport.getInt(1);
-						//System.out.println("**** Working");
+						SaveACC(workbook, AccData, accNumber, "Daily");
+						prevACChier = accountsForDailyReport.getInt(1);
+						// System.out.println("**** Working");
 					}
 				}
-			}catch(IOException ex) {
-	        	System.err.println("IOException information");
-	        	while (ex != null) {
+			} catch (IOException ex) {
+				System.err.println("IOException information");
+				while (ex != null) {
 					System.err.println("Error msg: " + ex.getMessage());
-					
+
 				}
-			}catch (SQLException ex) {
+			} catch (SQLException ex) {
 				System.err.println("SQLException information");
 				while (ex != null) {
 					System.err.println("Error msg: " + ex.getMessage());
@@ -217,11 +247,6 @@ public class ExcelSaveData {
 				}
 			}
 		}
-		}
-		
 	}
-	
-	
-	
 
-
+}
